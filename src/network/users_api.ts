e1b9f8c09c64015944934effeea024edb235967a
TatsianaYurchik <t26yurchik@gmail.com>
2023-03-03 02:53:11 +0300

    api_url changed

--- src/network/users_api.ts
+++ src/network/users_api.ts
@@ -2,8 +2,8 @@ import { ConflictError, UnauthorizedError } from "../errors/http_errors";
 import { UserNote } from "../models/userNote";
 import { User } from "../models/user";
 
-// const USER_API_URL = "https://collections-mern-api.onrender.com";
- const USER_API_URL = "http://localhost:8000";
+const USER_API_URL = "https://collections-mern-api.onrender.com";
+//  const USER_API_URL = "http://localhost:8000";
 
 async function fetchData(input: RequestInfo, init?: RequestInit, credentials?: RequestCredentials) {
     const response = await fetch(input, init );
